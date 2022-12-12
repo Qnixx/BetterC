86422608218264344fc2d48e8efd3693524b8c95
Ian-Marco-Moffett <spurious.vector@gmail.com>
2022-12-12 00:42:33 -0500

    Feat: Diagnosis.

---
+++ include/diag.h
@@ -0,0 +1,14 @@
+#ifndef DIAG_H_
+#define DIAG_H_
+
+#include <stdint.h>
+#include <stdarg.h>
+#include <context.h>
+
+
+void cc_diag_err(cc_context* cc_ctx, const char* fmt, ...);
+void cc_diag_warning(cc_context* cc_ctx, const char* fmt, ...);
+void cc_diag_note(cc_context* cc_ctx, const char* fmt, ...);
+
+
+#endif
---
+++ src/diag.c
@@ -0,0 +1,35 @@
+#include <diag.h>
+#include <stdio.h>
+
+
+static void diag_common(cc_context* cc_ctx, const char* fmt, va_list args) {
+  printf(fmt, args);
+  printf("%s: line %d\n", cc_ctx->current_filename, cc_ctx->current_line);
+}
+
+
+void cc_diag_err(cc_context* cc_ctx, const char* fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  printf("\e[0;31merror: ");
+  diag_common(cc_ctx, fmt, ap);
+  va_end(ap);
+}
+
+
+void cc_diag_warning(cc_context* cc_ctx, const char* fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  printf("\e[0;35mwarning: ");
+  diag_common(cc_ctx, fmt, ap);
+  va_end(ap);
+}
+
+
+void cc_diag_note(cc_context* cc_ctx, const char* fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+  printf("\e[0;36mnote: ");
+  diag_common(cc_ctx, fmt, ap);
+  va_end(ap);
+}

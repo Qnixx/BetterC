168cb3efb1565c5c67b3619e6924165c0652b245
Ian-Marco-Moffett <spurious.vector@gmail.com>
2022-12-12 00:42:49 -0500

    Feat: Symbol tools.

---
+++ include/symbol.h
@@ -0,0 +1,70 @@
+#ifndef SYMBOL_H_
+#define SYMBOL_H_
+
+#include <stddef.h>
+#include <stdint.h>
+
+// Structural types.
+typedef enum {
+  S_FUNCTION,
+  S_ARGUMENT,
+  S_VARIABLE
+} SYM_STYPE;
+
+// Primitive types.
+typedef enum {
+  P_NONE,
+  P_U8
+} SYM_PTYPE;
+
+struct symbol {
+  const char* name;
+  SYM_STYPE stype;
+  SYM_PTYPE ptype;
+  uint8_t is_global : 1;
+  struct symbol* local_symtbl;
+  struct symbol* parent;            // For local symbol.
+  size_t rbp_offset;                // For local symbols as well.
+  size_t local_symtbl_size;
+
+  // For functions.
+  size_t max_rbp;
+  size_t arg_count;
+
+  // For variables.
+  uint8_t is_initialized : 1;
+};
+
+
+void init_symtbls(void);
+
+/*
+ *  Pushes global symbol to the global
+ *  symbol table and returns the slot number.
+ *
+ */
+
+size_t symtbl_push_glob(const char* name, SYM_STYPE stype);
+
+/*
+ *  Pushes a local symbol to a global symbol's
+ *  local symbol table.
+ *
+ */
+
+size_t local_symtbl_push(struct symbol* glob, const char* name, SYM_STYPE stype, SYM_PTYPE ptype);
+
+/*
+ *  Returns -1 if not found, otherwise
+ *  returns slot.
+ *
+ */
+
+int64_t lookup_glob(const char* name);
+int64_t lookup_local(struct symbol* func, const char* name);
+
+void destroy_symbols(void);
+
+extern struct symbol* g_symtbl;
+
+#endif
---
+++ src/symbol.c
@@ -0,0 +1,89 @@
+#include <symbol.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#define ALIGN_UP(value, align)    (((value) + (align)-1) & ~((align)-1))
+
+
+struct symbol* g_symtbl = NULL;
+size_t g_symtbl_size = 0;
+
+
+void init_symtbls(void) {
+  g_symtbl = malloc(sizeof(struct symbol));
+}
+
+size_t symtbl_push_glob(const char* name, SYM_STYPE stype) {
+  g_symtbl[g_symtbl_size].name = strdup(name);
+  g_symtbl[g_symtbl_size].local_symtbl = NULL;
+  g_symtbl[g_symtbl_size].local_symtbl_size = 0;
+  g_symtbl[g_symtbl_size].rbp_offset = 0;
+  g_symtbl[g_symtbl_size++].stype = stype;
+  g_symtbl = realloc(g_symtbl, sizeof(struct symbol) * (g_symtbl_size + 2));
+  return g_symtbl_size - 1;
+}
+
+
+size_t local_symtbl_push(struct symbol* glob, const char* name, SYM_STYPE stype, SYM_PTYPE ptype) {
+  glob->local_symtbl[glob->local_symtbl_size].name = strdup(name);
+  glob->local_symtbl[glob->local_symtbl_size].stype = stype;
+  glob->local_symtbl[glob->local_symtbl_size].ptype = ptype;
+  glob->local_symtbl[glob->local_symtbl_size].parent = glob;
+
+  switch (ptype) {
+    case P_U8:
+      glob->max_rbp += 1;
+  }
+
+  // Stack must be 8 byte aligned or else
+  // you get a segfault on Linux.
+  glob->max_rbp = ALIGN_UP(glob->max_rbp, 8);
+
+  glob->local_symtbl[glob->local_symtbl_size].rbp_offset = glob->max_rbp+8;
+  glob->local_symtbl[glob->local_symtbl_size++].local_symtbl = NULL;
+  glob->local_symtbl = realloc(glob->local_symtbl, sizeof(struct symbol) * (glob->local_symtbl_size + 2));
+
+  return glob->local_symtbl_size - 1;
+}
+
+
+int64_t lookup_glob(const char* name) {
+  for (size_t i = 0; i < g_symtbl_size; ++i) {
+    if (strcmp(g_symtbl[i].name, name) == 0) {
+      return i;
+    }
+  }
+
+  return -1;
+}
+
+
+int64_t lookup_local(struct symbol* func, const char* name) {
+  for (size_t i = 0; i < func->local_symtbl_size; ++i) {
+    if (strcmp(func->local_symtbl[i].name, name) == 0) {
+      return i;
+    }
+  }
+
+  return -1;
+}
+
+
+void destroy_symbols(void) {
+  for (size_t i = 0; i < g_symtbl_size; ++i) {
+    if (g_symtbl != NULL) {
+      free((char*)g_symtbl[i].name);
+    }
+
+    for (size_t j = 0; j < g_symtbl[i].local_symtbl_size; ++j) {
+      if (g_symtbl[i].local_symtbl[j].name != NULL)
+        free((char*)g_symtbl[i].local_symtbl[j].name);
+    }
+
+    if (g_symtbl[i].local_symtbl != NULL) {
+      free(g_symtbl[i].local_symtbl);
+    }
+  }
+}
